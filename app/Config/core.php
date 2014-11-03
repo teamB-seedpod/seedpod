86082d69a481dc42063c5aefa34a7b174aa5da82
nakashi100 <tomohide.nakashima@gmail.com>
2014-11-03 11:09:49 +0800

    CAKE PHPの初期設定

--- app/Config/core.php
+++ app/Config/core.php
@@ -31,8 +31,8 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
-
+//	Configure::write('debug', 2);
+        Configure::write('debug', 1);
 /**
  * Configure the Error handler used to handle errors for your application. By default
  * ErrorHandler::handleError() is used. It will display errors using Debugger, when debug > 0
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'fe908a8dc41761c77cdf427cf388f4f74f4c6ca7');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '255664435118377356644702904674');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
