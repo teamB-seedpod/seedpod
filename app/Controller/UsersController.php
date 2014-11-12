2e06a28d6cfcac91f7218c084cabb2910d62cfe2
Tamiiy <vagrant@localhost.localdomain>
2014-11-12 03:07:58 +0000

    ユーザdetailとeditを整形。editが未完成状態

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -90,13 +90,14 @@ class UsersController extends AppController {
 		if ($this->request->is(array('post', 'put'))) {
 			if ($this->User->save($this->request->data)) {
 				$this->Session->setFlash(__('The user has been saved.'));
-				return $this->redirect(array('action' => 'index'));
+				return $this->redirect(array('action' => 'view'));
 			} else {
 				$this->Session->setFlash(__('The user could not be saved. Please, try again.'));
 			}
 		} else {
 			$options = array('conditions' => array('User.' . $this->User->primaryKey => $id));
-			$this->request->data = $this->User->find('first', $options);
+            $this->request->data = $this->User->find('first', $options);
+            //$this->set('user', $this->request->data);
 		}
 	}
 
